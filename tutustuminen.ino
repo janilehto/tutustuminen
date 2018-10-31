ba80085ff8d46498b6abc1dc80866876c1a7b192
Jani Lehto <jani.lehto@ictpro.labra>
2018-10-31 13:16:57 +0100

    Painikkeiden käsittely tuotu projektiin

--- tutustuminen.ino
+++ tutustuminen.ino
@@ -1,28 +1,48 @@
 #define LED1 12
+#define PAINIKE 5
 
-void setup() {
-  //Määritetään pinni lähdöksi
-  pinMode(LED1, OUTPUT);
-  //Alustetaan sarjaportti
-  Serial.begin(9600);
-}
-
+#define BUTTONPRESSED LOW
 #define LEDON LOW
 #define LEDOFF HIGH
 
-void loop() {
+//Luodaan globaali muuttuja ylläpitämään tietoa ledin tilasta
+int led1State=LEDOFF;
+//Luodaan globaali muuttuja painikkeen "vanhaa" tilaa varten
+int butOldState;
 
-  //Tutkitaan oliko sarjaportissa mitään
-  if(Serial.available() > 0){
-    //Luetaan yksi merkki sarjaportista
-    int x = Serial.read();
-    //Tarkistetaan oliko sarjaporttiin saapunut 'S' kirjain
-    if(x == 'S'){
-      //Jos merkki on 'S' sytytetään ledi
-      digitalWrite(LED1, LEDON);
-    }else if(x == 'R'){
-      //Ellei merkki ollut 'S' mutta on 'R' sammutetaan ledi
-      digitalWrite(LED1, LEDOFF);
+void setup() {
+  //Määritetään ledin liitos
+  pinMode(LED1, OUTPUT);
+  //Ohjataan ledi tilamuuttujan mukaiseen tilaan
+  digitalWrite(LED1, led1State);
+  //Määritetään painikkeen liitos
+  pinMode(PAINIKE, INPUT_PULLUP);
+  //Luetaan painikkeen tila "vanhaksi" tiedoksi
+  butOldState = digitalRead(PAINIKE);
+  //Alustetaan sarjaportti
+  Serial.begin(9600);
+}
+//Muuttuja suodatukselle
+#define FILTER 20
+void loop() {
+  //Luetaan painikkeen tila
+  int butNewState = digitalRead(PAINIKE);
+  //Jos uusi tila on erilainen kuin vanha on painikkeen tila muuttunut
+  if(butNewState != butOldState){
+    //Jos uusi tila edustaa painettu painiketta on aika reagoida
+    if(butNewState == BUTTONPRESSED){
+      //Vaihdetaan ledin tilatiedon tila
+      if(led1State==LEDOFF){
+        led1State=LEDON;
+      }else{
+        led1State=LEDOFF;
+      }
+      //Ohjataan ledi tilamuuttujan mukaiseen tilaan
+      digitalWrite(LED1, led1State);
     }
+    //Suodatetaan värähtelyt sisääntulosta
+    delay(FILTER);
   }
+  //Päivitetään tilamuuttujan arvo seuraavaa kierrosta varten
+  butOldState=butNewState;
 }
