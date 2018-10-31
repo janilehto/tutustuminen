6534368e6047525df4ce647f3306e2e107447f25
Jani Lehto <jani.lehto@ictpro.labra>
2018-10-31 13:12:26 +0100

    Sarjaportista luku aloitettu

--- tutustuminen.ino
+++ tutustuminen.ino
@@ -1,52 +1,27 @@
-/*
- * Blink esimerkkiohjelma esittää Arduino-ympäristön perusrakenteen. Ohjelma ohjaa arduino 
- * levyllä olevaa loistediodia,lediä vuoroin päälle ja pois, jääden hetkeksi pitämään lediä 
- * asetetussa tilassa. 
- */
+#define LED1 12
 
-/* 
- * Vakioilla "tuodaan" fyysinen kytkentä ohjelmaan. Eli levyllä oleva ledi on käytännössä 
- * kytkettynä mikro-ohjaimen liitäntään (pinniin) jonka numero on 13.
- */
-#define LED 13
-
-/*
- * setup() funktio ajetaan ainoastaan kun laite käynnistyy. Funktiossa on täten kätevää 
- * konfiguroida laitteen pinnit kytkennän mukaisesti. Tässä ohjelmassa pinni 13 tulee 
- * toimimaan lähtönä, eli mikro-ohjain tulee ohjaamaan pinnille sähköjä päälle ja pois. 
- */
 void setup() {
-  /*
-   * Alla olevalla funktiokutsulla konfiguroidaan pinni 13 lähdöksi (OUTPUT)
-   */
-  pinMode(LED, OUTPUT);
+  //Määritetään pinni lähdöksi
+  pinMode(LED1, OUTPUT);
+  //Alustetaan sarjaportti
   Serial.begin(9600);
 }
-/*
- * Luodaan muuttuja johon tallennetaan millisekuntimäärä jolla valitaan viiveen pituus.
- */
-int viive = 1000;
-/*
- * loop() funktio tahdittaa Arduinolaitteen toiminnan. Mikro-ohjain lukee joka rivin loop() 
- * funktion sisältä ja suorittaa käskyt järjestyksessä ylhäätä alas. Kun kaikki
- * käskyt on suoritettu aloittaa laite käskyjen suorittamisen loop() funktion alusta.
- */
-void loop(){
-  /*
-   * digitalWrite() funktiokutsulla vaikutetaan tietyn pinnin jännitteeseen. Alla oleva 
-   * funktiokutsu nostaa pinnin 13 jännitteen viiteen volttiin. Tämä jännite saa 
-   * ledin syttymään.
-   */
-   digitalWrite(LED, HIGH);
-   Serial.println("Ledi loistaa");
-   /*
-    * delay() funktiokutsulla saadaan laite pysähtymään halutuksi ajaksi. Haluttu 
-    * aika välitetään delay() funktiolle parametrillä, sulkujen välissä.
-    */
-    delay(viive);
-    // Sammutetaan ledi
-    digitalWrite(LED, LOW);
-    Serial.println("Ledi ei loista");
-    // Pysäytetään laite
-    delay(viive);
+
+#define LEDON LOW
+#define LEDOFF HIGH
+
+void loop() {
+  //Luetaan yksi merkki sarjaportista
+  int x = Serial.read();
+  //Tutkitaan oliko sarjaportissa mitään
+  if(x != -1){
+    //Tarkistetaan oliko sarjaporttiin saapunut 'S' kirjain
+    if(x == 'S'){
+      //Jos merkki on 'S' sytytetään ledi
+      digitalWrite(LED1, LEDON);
+    }else if(x == 'R'){
+      //Ellei merkki ollut 'S' mutta on 'R' sammutetaan ledi
+      digitalWrite(LED1, LEDOFF);
+    }
+  }
 }
