df6f593c2e4b96b00d6b831bca95d4b14257cbeb
Jani Lehto <jani.lehto@ictpro.labra>
2018-10-31 13:25:02 +0100

    Viimeinen tehtävä

--- tutustuminen.ino
+++ tutustuminen.ino
@@ -1,5 +1,6 @@
 #define LED1 12
-#define PAINIKE 5
+#define S1 5
+#define S2 4
 
 #define BUTTONPRESSED LOW
 #define LEDON LOW
@@ -8,7 +9,7 @@
 //Luodaan globaali muuttuja ylläpitämään tietoa ledin tilasta
 int led1State=LEDOFF;
 //Luodaan globaali muuttuja painikkeen "vanhaa" tilaa varten
-int butOldState;
+int S1OldState, S2OldState;
 
 void setup() {
   //Määritetään ledin liitos
@@ -16,19 +17,21 @@ void setup() {
   //Ohjataan ledi tilamuuttujan mukaiseen tilaan
   digitalWrite(LED1, led1State);
   //Määritetään painikkeen liitos
-  pinMode(PAINIKE, INPUT_PULLUP);
+  pinMode(S1, INPUT_PULLUP);
+  pinMode(S2, INPUT_PULLUP);  
   //Luetaan painikkeen tila "vanhaksi" tiedoksi
-  butOldState = digitalRead(PAINIKE);
+  S1OldState = digitalRead(S1);
+  S2OldState = digitalRead(S2);
   //Alustetaan sarjaportti
   Serial.begin(9600);
 }
 //Muuttuja suodatukselle
 #define FILTER 20
-void loop() {
+void handleS1() {
   //Luetaan painikkeen tila
-  int butNewState = digitalRead(PAINIKE);
+  int butNewState = digitalRead(S1);
   //Jos uusi tila on erilainen kuin vanha on painikkeen tila muuttunut
-  if(butNewState != butOldState){
+  if(butNewState != S1OldState){
     //Jos uusi tila edustaa painettu painiketta on aika reagoida
     if(butNewState == BUTTONPRESSED){
       //Vaihdetaan ledin tilatiedon tila
@@ -44,5 +47,28 @@ void loop() {
     delay(FILTER);
   }
   //Päivitetään tilamuuttujan arvo seuraavaa kierrosta varten
-  butOldState=butNewState;
+  S1OldState=butNewState;
+}
+
+void handleS2() {
+  //Luetaan painikkeen tila
+  int butNewState = digitalRead(S2);
+  //Jos uusi tila on erilainen kuin vanha on painikkeen tila muuttunut
+  if(butNewState != S2OldState){
+    //Jos uusi tila edustaa painettu painiketta on aika reagoida
+    if(butNewState == BUTTONPRESSED){
+      Serial.println("Morjens");
+    }
+    //Suodatetaan värähtelyt sisääntulosta
+    delay(FILTER);
+  }
+  //Päivitetään tilamuuttujan arvo seuraavaa kierrosta varten
+  S2OldState=butNewState;
+}
+
+
+
+void loop(){
+  handleS1();
+  handleS2();
 }
